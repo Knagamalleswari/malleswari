70e50ec601e765dcb95cc3368fc3c41ebf2f9863
unknown <kamathamnagamalleswari@gmail.com>
2023-03-02 21:46:07 +0530

    userlist component operator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
